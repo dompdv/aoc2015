74e28d684a1dd68c79baab9d067bc9b9ce4dddb4
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-11-13 18:22:07 +0100

    Day1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,21 @@
 defmodule AdventOfCode.Day01 do
+  import Enum
+
   def part1(args) do
+    counts = args |> to_charlist() |> frequencies()
+    counts[40] - counts[41]
+  end
+
+  def to_basement(_, -1, position), do: position
+
+  def to_basement([action | actions], level, position) do
+    case action do
+      40 -> to_basement(actions, level + 1, position + 1)
+      41 -> to_basement(actions, level - 1, position + 1)
+    end
   end
 
   def part2(args) do
+    to_basement(args |> to_charlist(), 0, 0)
   end
 end

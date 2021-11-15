f9c5284422ba95eac70f9fdd090216357012d35e
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-11-15 21:00:40 +0100

    day05 p1

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -1,5 +1,19 @@
 defmodule AdventOfCode.Day05 do
-  def part1(_args) do
+  import Enum
+
+  @vowels MapSet.new('aeiou')
+  @caracters ~w"aa bb cc dd ee ff gg hh ii jj kk ll mm nn oo pp qq rr ss tt uu vv ww xx yy zz"
+  @to_avoid ~w"ab cd pq xy"
+
+  def check_conditions(args) do
+    string = to_charlist(args)
+    vowels = (filter(string, fn c -> MapSet.member?(@vowels, c) end) |> count()) >= 3
+    double = String.contains?(args, @caracters)
+    avoid = not String.contains?(args, @to_avoid)
+    if vowels and double and avoid, do: 1, else: 0
+  end
+  def part1(args) do
+    args |> String.split("\n") |> drop(-1) |> map(&check_conditions/1) |> sum()
   end
 
   def part2(_args) do

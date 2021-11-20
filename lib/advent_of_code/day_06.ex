af92161e8f7bda913095a88d82d9c4ac98a7d871
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-11-20 17:49:54 +0100

    d06 p1

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,5 +1,34 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  import Enum
+
+  def update_lights(line, lights_on) do
+    IO.inspect(line)
+    %{"hx" => hx, "hy" => hy, "lx" => lx, "ly" => ly, "onoff" => onoff} =
+      Regex.named_captures(
+        ~r/(?<onoff>turn on|turn off|toggle) (?<hx>[0-9]*),(?<hy>[0-9]*) through (?<lx>[0-9]*),(?<ly>[0-9]*)/,
+        line
+      )
+    hx = String.to_integer(hx)
+    hy = String.to_integer(hy)
+    lx = String.to_integer(lx)
+    ly = String.to_integer(ly)
+    g = for x <- lx..hx, y <- ly..hy, into: MapSet.new(), do: {x,y}
+    case onoff do
+    "turn on" -> MapSet.union(lights_on, g)
+    "turn off" -> MapSet.difference(lights_on, g)
+    "toggle" -> to_switch_off = MapSet.intersection(lights_on, g)
+                to_switch_on = MapSet.difference(g, to_switch_off)
+                MapSet.union(MapSet.difference(lights_on, to_switch_off), to_switch_on)
+    end
+  end
+
+  def part1(args) do
+    reduce(
+      args |> String.split("\n") |> drop(-1),
+      MapSet.new(),
+      &update_lights/2
+    )
+    |> count()
   end
 
   def part2(_args) do
--- lib/mix/tasks/d06.p1.ex
+++ lib/mix/tasks/d06.p1.ex
@@ -5,7 +5,15 @@ defmodule Mix.Tasks.D06.P1 do
 
   @shortdoc "Day 06 Part 1"
   def run(args) do
-    input = nil
+    input =AdventOfCode.Input.get!(6, 2015)
+
+"""
+turn on 0,0 through 3,3
+turn off 1,1 through 2,2
+toggle 2,2 through 3,3
+"""
+
+      #AdventOfCode.Input.get!(6, 2015) #"turn on 498,499 through 500,501\n"
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),

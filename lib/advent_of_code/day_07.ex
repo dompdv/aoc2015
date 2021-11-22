36b30fa0366b33debcf8c54c9ad708a75c19e717
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-11-22 22:15:22 +0100

    d07 p1 done

--- lib/advent_of_code/day_07.ex
+++ lib/advent_of_code/day_07.ex
@@ -5,101 +5,106 @@ defmodule AdventOfCode.Day07 do
 
   def to_16bits(v), do: rem(rem(v, @bin16max) + @bin16max, @bin16max)
 
-  def execute_circuit({:value, wire_out, val}, c), do: Map.put(c, wire_out, val)
+  def execute_opcode(opcode, value_a, value_b) do
+    case opcode do
+      :and -> value_a &&& value_b
+      :or -> value_a ||| value_b
+      :lshift -> value_a <<< value_b
+      :rshift -> value_a >>> value_b
+    end
+  end
 
-  def execute_circuit({:connect, wire_out, wire_from}, c), do: Map.put(c, wire_out, c[wire_from])
+  def execute_circuit(_instructions, {opcode, {:val, value}}, c, wire_out) do
+    case opcode do
+      :connect -> Map.put(c, wire_out, value)
+      :not -> Map.put(c, wire_out, ~~~value)
+    end
+  end
 
-  def execute_circuit({:and, wire_out, wire_a, wire_b}, c),
-    do: Map.put(c, wire_out, c[wire_a] &&& c[wire_b])
+  def execute_circuit(instructions, {opcode, {:wire, wire_from}}, c, wire_out) do
+    c = wire_value(c, instructions, wire_from)
 
-  def execute_circuit({:or, wire_out, wire_a, wire_b}, c),
-    do: Map.put(c, wire_out, c[wire_a] ||| c[wire_b])
+    case opcode do
+      :connect -> Map.put(c, wire_out, c[wire_from])
+      :not -> Map.put(c, wire_out, ~~~c[wire_from])
+    end
+  end
 
-  def execute_circuit({:lshift, wire_out, wire_a, value}, c),
-    do: Map.put(c, wire_out, c[wire_a] <<< value)
+  def execute_circuit(_instructions, {opcode, {:val, value_a}, {:val, value_b}}, c, wire_out) do
+    Map.put(c, wire_out, execute_opcode(opcode, value_a, value_b))
+  end
 
-  def execute_circuit({:rshift, wire_out, wire_a, value}, c),
-    do: Map.put(c, wire_out, c[wire_a] >>> value)
+  def execute_circuit(instructions, {opcode, {:val, value_a}, {:wire, wire_b}}, c, wire_out) do
+    c = wire_value(c, instructions, wire_b)
+    Map.put(c, wire_out, execute_opcode(opcode, value_a, c[wire_b]))
+  end
 
-  def execute_circuit({:not, wire_out, wire_a}, c), do: Map.put(c, wire_out, ~~~c[wire_a])
+  def execute_circuit(instructions, {opcode, {:wire, wire_a}, {:val, value_b}}, c, wire_out) do
+    c = wire_value(c, instructions, wire_a)
+    Map.put(c, wire_out, execute_opcode(opcode, c[wire_a], value_b))
+  end
+
+  def execute_circuit(instructions, {opcode, {:wire, wire_a}, {:wire, wire_b}}, c, wire_out) do
+    c = wire_value(c, instructions, wire_a)
+    c = wire_value(c, instructions, wire_b)
+    Map.put(c, wire_out, execute_opcode(opcode, c[wire_a], c[wire_b]))
+  end
+
+  def wire_value(circuit_state, instructions, wire) do
+    if Map.has_key?(circuit_state, wire) do
+      circuit_state
+    else
+      execute_circuit(instructions, instructions[wire], circuit_state, wire)
+    end
+  end
 
   def part1(args) do
-    instructions = parse(args)
+    instructions =
+      parse(args)
+      # |> Enum.each(&IO.inspect/1)
+      |> Map.new()
 
-    Enum.reduce(
-      instructions,
-      %{},
-      &execute_circuit/2
-    )
+    res = wire_value(%{}, instructions, "a")
+    res["a"]
   end
 
   def part2(_args) do
   end
 
+  def to_v(value) do
+    if Integer.parse(value) == :error, do: {:wire, value}, else: {:val, String.to_integer(value)}
+  end
+
   def parse_line(line) do
-    IO.inspect(line)
     [action, wire_out] = String.split(line, " -> ")
 
     cond do
       String.contains?(action, " AND ") ->
         [wire_a, wire_b] = String.split(action, " AND ")
-        {:and, wire_out, wire_a, wire_b}
+        {wire_out, {:and, to_v(wire_a), to_v(wire_b)}}
 
       String.contains?(action, " OR ") ->
         [wire_a, wire_b] = String.split(action, " OR ")
-        {:or, wire_out, wire_a, wire_b}
+        {wire_out, {:or, to_v(wire_a), to_v(wire_b)}}
 
       String.contains?(action, " LSHIFT ") ->
         [wire_a, val] = String.split(action, " LSHIFT ")
-        {:lshift, wire_out, wire_a, String.to_integer(val)}
+        {wire_out, {:lshift, to_v(wire_a), to_v(val)}}
 
       String.contains?(action, " RSHIFT ") ->
         [wire_a, val] = String.split(action, " RSHIFT ")
-        {:rshift, wire_out, wire_a, String.to_integer(val)}
+        {wire_out, {:rshift, to_v(wire_a), to_v(val)}}
 
       String.contains?(action, "NOT ") ->
         [_, wire_a] = String.split(action, "NOT ")
-        {:not, wire_out, wire_a}
-
-      Integer.parse(action) ->
-        {:connect, wire_out, action}
+        {wire_out, {:not, to_v(wire_a)}}
 
       true ->
-        {:value, wire_out, String.to_integer(action)}
+        {wire_out, {:connect, to_v(action)}}
     end
   end
 
   def parse(args) do
     args |> String.split("\n") |> Enum.drop(-1) |> Enum.map(&parse_line/1)
-  end
-
-  def gate_set(c, wire, value), do: Map.put(c, wire, value)
-  def gate_and(c, wire, wire_a, wire_b), do: Map.put(c, wire, c[wire_a] &&& c[wire_b])
-  def gate_or(c, wire, wire_a, wire_b), do: Map.put(c, wire, c[wire_a] ||| c[wire_b])
-
-  def gate_lshift(c, wire, wire_a, value), do: Map.put(c, wire, c[wire_a] <<< value)
-  def gate_rshift(c, wire, wire_a, value), do: Map.put(c, wire, c[wire_a] >>> value)
-
-  def gate_not(c, wire, wire_a), do: Map.put(c, wire, ~~~c[wire_a])
-
-  def part1_old(_args) do
-    circuit = [
-      fn c -> gate_set(c, "x", 123) end,
-      fn c -> gate_set(c, "y", 456) end,
-      fn c -> gate_and(c, "d", "x", "y") end,
-      fn c -> gate_or(c, "e", "x", "y") end,
-      fn c -> gate_lshift(c, "f", "x", 2) end,
-      fn c -> gate_rshift(c, "g", "y", 2) end,
-      fn c -> gate_not(c, "h", "x") end,
-      fn c -> gate_not(c, "i", "y") end
-    ]
-
-    Enum.reduce(
-      circuit,
-      %{},
-      fn fun_to_apply, wires_status ->
-        fun_to_apply.(wires_status)
-      end
-    )
   end
 end

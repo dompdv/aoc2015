36af019e6d8237c94b37a52408ce2f7cd65d70c8
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-11-23 20:30:07 +0100

    d08

--- lib/advent_of_code/day_08.ex
+++ lib/advent_of_code/day_08.ex
@@ -1,7 +1,40 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  import Enum
+
+  def rlength(line), do: rlength(line, 0)
+
+  def rlength([], l), do: l - 2
+  def rlength([?\\, ?\\ | r], l), do: rlength(r, l + 1)
+  def rlength([?\\, ?" | r], l), do: rlength(r, l + 1)
+  def rlength([?\\, ?x, _, _ | r], l), do: rlength(r, l + 1)
+  def rlength([_ | r], l), do: rlength(r, l + 1)
+
+  def part1(args) do
+    args
+    |> String.split("\n")
+    |> drop(-1)
+    |> map(fn line ->
+      cl = to_charlist(line)
+      count(cl) - rlength(cl)
+    end)
+    |> sum()
   end
 
-  def part2(_args) do
+  def encode(line), do: encode(line, 0)
+
+  def encode([], l), do: l + 2
+  def encode([?" | r], l), do: encode(r, l + 2)
+  def encode([?\\ | r], l), do: encode(r, l + 2)
+  def encode([_ | r], l), do: encode(r, l + 1)
+
+  def part2(args) do
+    args
+    |> String.split("\n")
+    |> drop(-1)
+    |> map(fn line ->
+      cl = to_charlist(line)
+      encode(cl) - count(cl)
+    end)
+    |> sum()
   end
 end
--- lib/mix/tasks/d08.p1.ex
+++ lib/mix/tasks/d08.p1.ex
@@ -5,7 +5,8 @@ defmodule Mix.Tasks.D08.P1 do
 
   @shortdoc "Day 08 Part 1"
   def run(args) do
-    input = nil
+    # "\"\"\n\"abc\"\n\"aaa\"aaa\"\n\"\\x27\"\n"
+    input = AdventOfCode.Input.get!(8, 2015)
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),

6726cc167a57d868ad38f3dbcc8afa9032cd4cd8
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2021-11-28 17:25:32 +0100

    d12 p2

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -12,6 +12,18 @@ defmodule AdventOfCode.Day12 do
     add_numbers(a)
   end
 
-  def part2(_args) do
+  def add_numbers2(s) when is_number(s), do: s
+  def add_numbers2(s) when is_bitstring(s), do: 0
+  def add_numbers2([]), do: 0
+  def add_numbers2(l) when is_list(l), do: map(l, fn v -> add_numbers2(v) end) |> sum()
+
+  def add_numbers2(m) when is_map(m) do
+    values = Map.values(m)
+    if Enum.member?(values, "red"), do: 0, else: map(values, fn v -> add_numbers2(v) end) |> sum()
+  end
+
+  def part2(args) do
+    {:ok, a} = Jason.decode(args)
+    add_numbers2(a)
   end
 end

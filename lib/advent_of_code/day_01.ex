63261192e1d2b4e987c36477b795b4e47268c7d8
Dominique Poudevigne <dominique.poudevigne@dataiku.com>
2022-08-25 13:20:51 +0200

    Day14 p1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -3,19 +3,17 @@ defmodule AdventOfCode.Day01 do
 
   def part1(args) do
     counts = args |> to_charlist() |> frequencies()
-    counts[40] - counts[41]
+    counts[?(] - counts[?)]
   end
 
   def to_basement(_, -1, position), do: position
 
   def to_basement([action | actions], level, position) do
     case action do
-      40 -> to_basement(actions, level + 1, position + 1)
-      41 -> to_basement(actions, level - 1, position + 1)
+      ?( -> to_basement(actions, level + 1, position + 1)
+      ?) -> to_basement(actions, level - 1, position + 1)
     end
   end
 
-  def part2(args) do
-    to_basement(args |> to_charlist(), 0, 0)
-  end
+  def part2(args), do: to_basement(args |> to_charlist(), 0, 0)
 end
--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -1,6 +1,28 @@
 defmodule AdventOfCode.Day14 do
-  def part1(_args) do
+  @exp_in ~r/([a-z,A-Z]+) can fly (\d+) km\/s for (\d+) seconds, but then must rest for (\d+) seconds./
+
+  def distance(t, speed, duration, rest) do
+    cycle = duration + rest
+    cycles = div(t, cycle) * speed * duration
+    after_cycles = min(rem(t, cycle), duration) * speed
+    cycles + after_cycles
   end
+
+  def distance(t, {who, speed, duration, rest}), do: {who, distance(t, speed, duration, rest)}
+
+  def parse_line(line) do
+    [_, who, speed, duration, rest] = Regex.run(@exp_in, line)
+    {who, String.to_integer(speed), String.to_integer(duration), String.to_integer(rest)}
+  end
+
+  def parse(args), do: args |> String.split("\n", trim: true) |> Enum.map(&parse_line/1)
+
+  def part1(args),
+    do:
+      parse(args)
+      |> Enum.map(fn reindeer -> distance(2503, reindeer) end)
+      |> Enum.map(&elem(&1, 1))
+      |> Enum.max()
 
   def part2(_args) do
   end
